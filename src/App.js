cfd35141996a7ea69b708a6d8252a7698e425aa2
Minh <minhkyuuby@gmail.com>
2019-03-20 21:20:51 +0700

    first-commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import Main from './components/MainComponent';
+import './App.css';
+import Head from "./components/HeaderComponent";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="masthead">
+          <Head/>
+          <div className="container h-100">
+            <div className="row h-100 align-items-center">
+              <div className="col-12 text-center">
+                <h1>Đoàn Văn Minh</h1>
+                <p className="lead text-white">- nếu không được sống vì đam mê cuộc đời sẽ trở lên thật trống rỗng -</p>
+                <img src="/asset/images/code.png" className="App-logo" alt=""/>
+              </div>
+            </div>
+          </div>
+        </header>
+        <body className="mt-3">
+          <Main/>
+        </body>
+        <div className="jumbotron mt-3">
+        <h5>Developed by Minh Doan Van</h5>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
